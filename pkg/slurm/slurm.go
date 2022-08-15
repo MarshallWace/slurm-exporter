d5f1e60d4f1a55d8d21a642c27a216b1123ee3e2
Giacomo Tirabassi <g.tirabassi@mwam.com>
2022-08-15 17:36:37 +0100

    fixed rebase

--- pkg/slurm/slurm.go
+++ pkg/slurm/slurm.go
@@ -111,11 +111,7 @@ func NewRegistry(gpuCollectorEnabled bool, exectimeout int) (*prometheus.Registr
 	if err != nil {
 		return nil, err
 	}
-<<<<<<< Updated upstream
 	err = reg.Register(NewJobsCollector(false)) // from jobs.go
-=======
-	err = reg.Register(NewJobsCollector(false)) // from scontrol.go
->>>>>>> Stashed changes
 	if err != nil {
 		return nil, err
 	}
